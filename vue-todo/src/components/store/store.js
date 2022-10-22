1b2d78252cb2fe3ce8a3ef510cbdc8fdc49b961d
JEONGA <devt3211@gmail.com>
2022-10-22 12:15:17 +0900

    modules폴더로 모듈화

--- vue-todo/src/components/store/store.js
+++ vue-todo/src/components/store/store.js
@@ -1,31 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters';
-import * as mutations from './mutations';
+import * as todoApp from '../modules/todoApp'
 
 Vue.use(Vuex);      // use: 전역으로 Vuex를 쓰겠다.
 
-const storage = {
-    fetch(){
-        const arr = [];
-        if (localStorage.length > 0) {
-			for (var i = 0; i < localStorage.length; i++) {
-				if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    // this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-			}
-		}
-        return arr;
-    },
-}
-
 export const store = new Vuex.Store({
-    state: {
-        headerText: 'TODO it!!',
-        todoItems : storage.fetch(),
-    },
-    getters: getters, 
-    mutations: mutations
-    
+    modules: {
+        todoApp
+    }
 })
